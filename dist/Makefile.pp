89a947a5ed20d3d69672b36fa301ad828f7eb3e5
hardaker <hardaker@bfbdd641-fcfc-42dc-b6c1-a158420a57ae>
2009-09-22 16:22:31 +0000

    r12311@wjh:  hardaker | 2009-08-17 16:37:08 -0700  fix packing so include a bunch of modules
    git-svn-id: file:///tmp/svktogit.17486/svnrepo-twitmail.git/trunk@51 bfbdd641-fcfc-42dc-b6c1-a158420a57ae

--- dist/Makefile.pp
+++ dist/Makefile.pp
@@ -1,3 +1,48 @@
 #!/bin/sh
 
-pp -o twitmail.`uname -s | tr A-Z a-z` twitmail
+if test ! -f Curses/UI/Language/english.pm ; then
+    mkdir -p Curses/UI/Language
+    cp -pr /usr/local/lib/perl5/site_perl/5.10.0/Curses/UI/Language/*.pm Curses/UI/Language/
+fi
+
+#   -M Curses::UI::Pop 
+
+pp -M Net::Twitter  -M Net::Twitter::Role::Legacy \
+   -M Net::Twitter::Role::API::REST \
+   -M Net::Twitter::Role::API::Search \
+   -M Net::Twitter::Role::API::TwitterVision \
+   -M Net::Twitter::Role::WrapError \
+   -M Net::Twitter::Role::OAuth \
+   -M Net::Twitter::Role::InflateObjects \
+   -M LWP::UserAgent \
+   -M Curses::UI::Buttonbox \
+   -M Curses::UI::Calendar \
+   -M Curses::UI::Checkbox \
+   -M Curses::UI::Color \
+   -M Curses::UI::Common \
+   -M Curses::UI::Container \
+   -M Curses::UI::Label \
+   -M Curses::UI::Language \
+   -M Curses::UI::Listbox \
+   -M Curses::UI::Menubar \
+   -M Curses::UI::Notebook \
+   -M Curses::UI::PasswordEntry \
+   -M Curses::UI::Progressbar \
+   -M Curses::UI::Radiobuttonbox \
+   -M Curses::UI::Searchable \
+   -M Curses::UI::TextEditor \
+   -M Curses::UI::TextEntry \
+   -M Curses::UI::TextViewer \
+   -M Curses::UI::Widget \
+   -M Curses::UI::Window \
+   -M Curses::UI::Dialog::Basic \
+   -M Curses::UI::Dialog::Calendar \
+   -M Curses::UI::Dialog::Dirbrowser \
+   -M Curses::UI::Dialog::Error \
+   -M Curses::UI::Dialog::Filebrowser \
+   -M Curses::UI::Dialog::Progress \
+   -M Curses::UI::Dialog::Question \
+   -M Curses::UI::Dialog::Status \
+   -M Curses::UI::Language::english \
+   -a Curses/UI/Language/english.pm \
+   -o twitmail.`uname -s | tr A-Z a-z` twitmail
